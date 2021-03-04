c2a589d128c8ca81d4607cdd68d3ed0c28347573
ShahbazyanS <armyanin098@mail.ru>
2021-03-04 11:22:54 +0400

    Request Model validation

--- src/main/java/online/shop/onlineshop/dto/UserDto.java
+++ src/main/java/online/shop/onlineshop/dto/UserDto.java
@@ -5,13 +5,23 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserDto {
+    @NotBlank(message = "Username is required")
     private String username;
+    @NotBlank(message = "Email is required")
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6,message = "Password lenght sould be at least 6 symbol")
     private String password;
+    @Size(min = 6,message = "Password lenght sould be at least 6 symbol")
     private String confirmPassword;
 }
--- src/main/java/online/shop/onlineshop/model/User.java
+++ src/main/java/online/shop/onlineshop/model/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Getter
@@ -24,16 +25,20 @@ public class User {
     private boolean active = false;
     @Enumerated(value = EnumType.STRING)
     private UserType userType = UserType.USER;
+    @NotNull
     @OneToOne
     private PersonalInfo personalInfo;
+    @NotNull
     @OneToOne
     private ShippingInfo shippingInfo;
 
+    @NotNull
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinColumn(name = "shopping_cart_id")
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private ShoppingCart shoppingCart;
 
+    @NotNull
     @OneToOne
     private Order order;
     private String token;
